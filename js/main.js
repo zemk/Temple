2d0a74d3e4e518f6d38589eef6c7b5f3b2a09e32
Vadim <88936621+zemk@users.noreply.github.com>
2021-11-07 16:21:33 +0300

    Temple-Pc

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+  autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  
+
+  
+});
